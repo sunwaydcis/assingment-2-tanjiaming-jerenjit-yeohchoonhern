440e3b20e89bf357c61c965aec4194caffca6a8a
jerencandy <jerencandy30@gmail.com>
2025-12-06 18:08:43 +0800

    booking case class added

--- src/main/scala/MostProfitable.scala
+++ src/main/scala/MostProfitable.scala
@@ -2,6 +2,12 @@ import scala.io.Source
 import scala.util.{Try, Success, Failure}
 
 object MostProfitable {
+
+  case class Booking(hotel: String, rooms: Int, profitMargin: Double)
+
   def main(args: Array[String]): Unit = {
   }
 }
+
+
+
